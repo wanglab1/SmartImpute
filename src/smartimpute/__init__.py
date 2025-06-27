2305434c13a001d9a835efeec3af70e8537bd118
wanglab1 <69062235+wanglab1@users.noreply.github.com>
2025-06-27 16:28:01 -0400

    Update __init__.py

--- src/smartimpute/__init__.py
+++ src/smartimpute/__init__.py
@@ -1,7 +1,7 @@
-from .generator import Generator
-from .discriminator import D1_WGAN_GP, D2_BCE
-from .utils         import gradient_penalty, recon_loss_fn
-from .smartimpute   import train_smartimpute
+from .generator      import Generator
+from .discriminator  import D1_WGAN_GP, D2_BCE
+from .utils          import gradient_penalty, recon_loss_fn
+from .smart          import train_smartimpute  # renamed from smartimpute.py → smart.py
 
 __all__ = [
     "Generator",
@@ -11,4 +11,3 @@ __all__ = [
     "recon_loss_fn",
     "train_smartimpute",
 ]
-
